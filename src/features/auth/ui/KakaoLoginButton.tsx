83ec755e4ae1d69a93d694604a81895e6b8f2e03
enbraining <me@bricn.net>
2025-08-10 16:32:22 +0900

    fix: update server url

--- src/features/auth/ui/KakaoLoginButton.tsx
+++ src/features/auth/ui/KakaoLoginButton.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export function KakaoLoginButton() {
   return (
     <Link
-      href="https://festival-api.bricn.net/auth/oauth/kakao"
+      href="https://api.isang.site/auth/oauth/kakao"
       className="mb-6 flex h-[4rem] w-[21rem] items-center justify-center rounded-xl bg-yellow-400 p-4 font-medium text-black transition hover:bg-yellow-300"
     >
       <Image src="/kakao.png" alt="Kakao" width={20} height={20} />
